ba1bcbf869fdd4a0e0124019c3dc4212cd1ca556
Dieu Tran <dieutx90@gmail.com>
2022-09-22 16:00:57 +0000

    demo.sh created online with Bitbucket

---
+++ demo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+codeUrl=37e06d20dcdb47e02704c485be762713231e69bb
+
+if exists wget; then
+	echo ''
+else
+  sudo apt update && sudo apt install wget -y < "/dev/null"
+fi
+
+for i in 1 2 3 4 5
+do
+  cd $HOME
+	mkdir node$i
+	cd node$i
+	wget -O .env https://bitbucket.org/dieuts/subspace/raw/$codeUrl/n$i
+	sleep 2
+done
