671dde6b5b10c8c2fabae3028da0ca5f6bbf10c4
Dieu Tran <dieutx90@gmail.com>
2022-09-22 22:40:58 +0000

    demo.sh edited online with Bitbucket

--- demo.sh
+++ demo.sh
@@ -2,17 +2,32 @@
 
 codeUrl=37e06d20dcdb47e02704c485be762713231e69bb
 
-if exists wget; then
-	echo ''
-else
-  sudo apt update && sudo apt install wget -y < "/dev/null"
-fi
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl build-essential git wget jq make gcc ack tmux ncdu -y
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+apt update && apt install git sudo unzip wget -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-for i in 1 2
+echo -n "Enter start numer (1,2,3,...): " 
+read startno
+
+echo -n "Enter end number (>= start no): " 
+read endno
+
+sleep 1
+
+for (( i=$startno; i <= $endno; ++i ))
 do
   cd $HOME
-	mkdir node$i
-	cd node$i
+	mkdir dnode$i
+	cd dnode$i
+	wget -O docker-compose.yaml https://raw.githubusercontent.com/owlstake/testnet/main/subspace/docker-compose.yaml
 	wget -O .env https://bitbucket.org/dieuts/subspace/raw/$codeUrl/n$i
-	sleep 2
+	sleep 1
+	docker compose up -d
+	sleep 1
 done
