2a68fb108a79df2206c4ed8ff16b0c7e5531a4db
Dieu Tran <dieutx90@gmail.com>
2022-09-22 23:08:13 +0000

    demo.sh edited online with Bitbucket

--- demo.sh
+++ demo.sh
@@ -3,7 +3,7 @@
 codeUrl=master
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl build-essential git wget jq make gcc ack tmux ncdu -y
+sudo apt install curl htop build-essential git wget jq make gcc ack tmux ncdu -y
 sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 && chmod +x /usr/local/bin/yq
 apt update && apt install git sudo unzip wget -y
 curl -fsSL https://get.docker.com -o get-docker.sh
