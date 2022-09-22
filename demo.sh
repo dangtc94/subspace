506b0f510d8cf5202034b1b9a3d36d9f50d2bcf2
Dieu Tran <dieutx90@gmail.com>
2022-09-22 23:32:22 +0000

    demo.sh edited online with Bitbucket

--- demo.sh
+++ demo.sh
@@ -6,11 +6,18 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install curl htop build-essential git wget jq make gcc ack tmux ncdu -y
 sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 && chmod +x /usr/local/bin/yq
 apt update && apt install git sudo unzip wget -y
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+if [[ $(which docker) && $(docker --version) ]]; then
+    echo "Docker installed!"
+    # command
+  else
+    echo "Installing docker..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+	sudo sh get-docker.sh
+	curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+	sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+fi
 
 echo -n "Enter start numer (1,2,3,...): " 
 read startno
