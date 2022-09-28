fd717084b5a136b71f4400eb74b12b9806dafbaf
Do Quang Hien <dieutx90@gmail.com>
2022-09-28 12:49:04 +0700

    adad

--- edit node vultr.sh
+++ edit node vultr.sh
@@ -15,11 +15,9 @@ lsof -i -P -n | grep LISTEN
 docker compose logs -f --tail=100
 
  
-<<<<<<< HEAD
-wget -O subspace.sh https://bitbucket.org/dieuts/subspace/raw/master/subspace.sh && chmod +x subspace.sh && ./subspace.sh 111 112
-=======
+
 wget -O subspace.sh https://bitbucket.org/dieuts/subspace/raw/master/subspace.sh && chmod +x subspace.sh && ./subspace.sh 111 113
->>>>>>> 66023235d993bef0a25e9a9176a3e5f2a9ced9dd
+
 
 wget -O subspace.sh https://bitbucket.org/dieuts/subspace/raw/master/tools/generate_subspace_wallet_subkey.sh && chmod +x subspace.sh && ./subspace.sh
 
