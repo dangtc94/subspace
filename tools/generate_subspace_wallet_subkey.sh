383b1158b0b4632102089b33370eae8e51558868
Do Quang Hien <dieutx90@gmail.com>
2022-10-01 11:32:06 +0700

    asdasd

--- tools/generate_subspace_wallet_subkey.sh
+++ tools/generate_subspace_wallet_subkey.sh
@@ -1,5 +1,6 @@
+  GNU nano 4.8                                                                          gen-wallets.sh
 #!/bin/bash
-echo -e "Number of wallets (>=1): " 
+echo -e "Number of wallets (>=1): "
 read noofwallet
 x=1
 while [ $x -le $noofwallet ]
@@ -8,17 +9,17 @@ do
   subkey generate -n subspace_testnet
   #echo -e "\n"
   x=$(( $x + 1 ))
-done > number_of_wallets.txt
-
+done > rewardaddress-phrases.txt
+x=1
 while [ $x -le $noofwallet ]
 do
   #echo "account $x:"
-  echo sed 8*$x'!d' number_of_wallets.txt 
+  echo $(sed $((8*$x))'!d' number_of_wallets.txt)
   #echo -e "\n"
   x=$(( $x + 1 ))
-done #> res.txt
-#v1="SS58 Address:"
-#v2=" "
-#v3=""
-#sed -i "s/$v1/$v3/" res.txt
-#sed -i "s/$v2/$v3/" res.txt
+done > rewardaddress.txt
+v1="SS58 Address:"
+v2=" "
+v3=""
+sed -i "s/$v1/$v3/" rewardaddress.txt
+sed -i "s/$v2/$v3/" rewardaddress.txt
