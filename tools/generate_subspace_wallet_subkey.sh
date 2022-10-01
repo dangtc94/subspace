3461d7224bc7ae5e4c603becb26d04eddd18ed45
Do Quang Hien <dieutx90@gmail.com>
2022-10-01 11:14:16 +0700

    sfsd

--- tools/generate_subspace_wallet_subkey.sh
+++ tools/generate_subspace_wallet_subkey.sh
@@ -6,6 +6,19 @@ while [ $x -le $noofwallet ]
 do
   echo "account $x:"
   subkey generate -n subspace_testnet
-  echo -e "\n"
+  #echo -e "\n"
   x=$(( $x + 1 ))
 done > number_of_wallets.txt
+
+while [ $x -le $noofwallet ]
+do
+  #echo "account $x:"
+  sed '8*$x!d' number_of_wallets.txt 
+  #echo -e "\n"
+  x=$(( $x + 1 ))
+done > res.txt
+v1="SS58 Address:"
+v2=" "
+v3=""
+sed -i "s/$v1/$v3/" res.txt
+sed -i "s/$v2/$v3/" res.txt
